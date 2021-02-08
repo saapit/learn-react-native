194e7ec728ad96ba2c8b4862fc281f5b2acd6105
sapit <syafiqshahrim19@gmail.com>
2021-02-08 16:16:59 +0800

    Show the result called API  to RN application

--- src/pages/CallAPIVanilla/index.js
+++ src/pages/CallAPIVanilla/index.js
@@ -1,21 +1,44 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Button, Image, Text, View} from 'react-native';
 
 const CallAPIVanilla = () => {
+  const [dataUser, setDataUser] = useState({
+    avatar: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+  });
+
+  const [dataJob, setDataJob] = useState({
+    name: '',
+    job: '',
+  });
   useEffect(() => {
     //   Call API method GET
     // fetch('https://reqres.in/api/users/2')
     //   .then((response) => response.json())
     //   .then((json) => console.log(json));
+  }, []);
 
+  const getData = () => {
+    fetch('https://reqres.in/api/users/2')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setDataUser(json.data);
+      });
+  };
+
+  const postData = () => {
     // Call API methid POST
     const dataForAPI = {
       name: 'morpheus',
       job: 'leader',
     };
 
-    console.log('data object:', dataForAPI);
-    console.log('data stringify:', JSON.stringify(dataForAPI));
+    // console.log('data object:', dataForAPI);
+    // console.log('data stringify:', JSON.stringify(dataForAPI));
+
     fetch('https://reqres.in/api/users', {
       method: 'POST',
       headers: {
@@ -26,11 +49,25 @@ const CallAPIVanilla = () => {
       .then((response) => response.json())
       .then((json) => {
         console.log('post response: ', json);
+        setDataJob(json);
       });
-  }, []);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Call API Vanilla</Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Call API Vanilla</Text>
+      </View>
+      <Button title="GET DATA" onPress={getData} />
+      <Text style={styles.title}>Response GET Data</Text>
+      <Image source={{uri: dataUser.avatar}} style={styles.avatar} />
+      <Text>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
+      <Text>{dataUser.email}</Text>
+      <View style={styles.line} />
+      <Button title="POST DATA" onPress={postData} />
+      <Text style={styles.title}>Response POST DATA</Text>
+      <Text>{dataJob.name}</Text>
+      <Text>{dataJob.job}</Text>
     </View>
   );
 };
@@ -45,4 +82,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {textAlign: 'center'},
+  line: {height: 2, backgroundColor: 'black', marginVertical: 20},
+  avatar: {width: 100, height: 100, borderRadius: 100},
 });
--- src/pages/ReactNativeSvg/index.js
+++ src/pages/ReactNativeSvg/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import UndrawMyApp from '../../assets/images/undraw_my_app_re_gxtj.svg';
 
 const ReactNativeSvg = () => {
